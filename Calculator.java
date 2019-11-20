3107300cf3a961cb8935929b82c08a5ed67ed719
mrathne <56542226+mrathne@users.noreply.github.com>
2019-11-20 16:47:04 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+class Calculator implements CalculatorInterface{
+   public int addition(int firtValue, int secondValue) {
+   }
+   public int substraction(int firtValue, int secondValue){
+   }
+   public double division(int firtValue, int secondValue){
+   }
+   public int multification(int firtValue, int secondValue){
+   }
+}
