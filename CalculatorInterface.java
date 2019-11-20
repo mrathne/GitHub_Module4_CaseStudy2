b794185e268089159d77093471a545c273320e20
mrathne <56542226+mrathne@users.noreply.github.com>
2019-11-20 16:47:35 +0530

    Create CalculatorInterface.java

---
+++ CalculatorInterface.java
@@ -0,0 +1,6 @@
+interface CalculatorInterface{
+   public int addition(int firtValue, int secondValue);
+   public int substraction(int firtValue, int secondValue);
+   public double division(int firtValue, int secondValue);
+   public int multification(int firtValue, int secondValue);
+}
